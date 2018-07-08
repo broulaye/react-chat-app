e30767236f558c44e299cdd7e3b12366be16e4e4
Broulaye Doumbia <Broulaye.Doumbia@ibm.com@Broulayes-MacBook-Pro-2.local>
2018-07-07 22:43:02 -0500

    initial HTML structure of chat app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {sidebarOpen: false};
+
+    // This binding is necessary to make `this` work in the callback
+    this.openNav = this.openNav.bind(this);
+    this.closeNav = this.closeNav.bind(this);
+  }
+
+  openNav() {
+    console.log('opening Nav');
+    // debugger
+    // document.getElementsByClassName("chat-sidebar")[0].style.width = "25%";
+    const open = this.state.sidebarOpen;
+    this.setState({ sidebarOpen: !open });
+  }
+
+  closeNav() {
+    console.log('closing Nav');
+    this.setState({ sidebarOpen: false })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="chat-root">
+          <div className="chat-sidebar" sidebar-open={this.state.sidebarOpen.toString()}>
+            <div className="chat-sidebar-header"></div>
+          </div>
+          <div className="chat-container">
+            <div className="chat-container-header">
+              <button type="button" className="chat-list-button" onClick={this.openNav}>Toggle</button>
+            </div>
+            <div className="chat-container-content" onClick={this.closeNav}></div>
+          </div>
+        </div>
       </div>
     );
   }
